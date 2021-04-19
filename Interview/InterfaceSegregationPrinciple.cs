59cb491a7d48541f7eb8a1c87787e280c078cfbd
rajugaddam1224 <gaddam_raju@outlook.com>
2021-04-19 20:18:16 +0530

    changes done

--- Interview/InterfaceSegregationPrinciple.cs
+++ Interview/InterfaceSegregationPrinciple.cs
@@ -8,12 +8,62 @@ using System.Threading.Tasks;
  * One fat interface need to be split to  many smaller and relevant interfaces 
    so that clients can know about the interfaces that are relevant to them
  * The ISP was first used and formulated by Rober C.Martin wle consulting Xerox.
+ * 
+ * 
+ *  * Xerox had created a new printer system that could perform a variety of tasks 
+  such as stapling and faxing along with the regular printing task.
+ * the software for this system was created from the ground up
+ * modification and deployment to the system became more complex.
+ * One large Job class is segregated to multiple interfaces depending on the requirement.
  */
+
+
+
 namespace Interview
 {
-   public class InterfaceSegregationPrinciple
+    public class HPScan : IPrintScanContent
     {
+        public bool ColorPrintContent(string content)
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool PrintContent(string content)
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool PrintScan(string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class CanonPrinter : IPrintScanContent,IColorPrint
+    {
+        public bool ColorPrint(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PrintContent(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PrintScan(string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    interface IPrintScanContent
+    {
+        bool PrintContent(string content);
+        bool PrintScan(string content);
+
+    }
+
+    interface IColorPrint
+        {
+        bool ColorPrint(string content);
     }
 }
